98537370d5b2f98b01da0158991232f65b1485f8
Laplace <johnalexisbuot@gmail.com>
2021-08-30 15:27:55 +0800

    ADDED: Handler for getting character Data

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,10 +1,17 @@
 import { scraper } from "../utils/scraper";
 import { Handler } from "express";
-import { parseCharactersPageData } from "./../services/character.service";
 import { baseUrlEndpoint } from "../utils/constants";
+import { parseCharactersPageData, parseCharacterPageData } from "./../services/character.service";
 
 export const getCharacters: Handler = async (_, res) => {
     const data = await scraper(`${baseUrlEndpoint}/db/char/characters/?lang=EN`, parseCharactersPageData);
+
+    res.send(data);
+};
+
+export const getCharacter: Handler = async (req, res) => {
+    const { char } = req.params;
+    const data = await scraper(`${baseUrlEndpoint}/db/char/${char}/?lang=EN`, parseCharacterPageData);
 
     res.send(data);
 };
